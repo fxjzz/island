04ad8bc61a59b9ca18885bfa04c4998f242dcb9a
fxjzz <1042183935@qq.com>
2023-10-05 10:00:42 +0800

    home 页搭建

--- src/theme-default/components/Nav/index.tsx
+++ src/theme-default/components/Nav/index.tsx
@@ -17,33 +17,35 @@ export function Nav() {
   const { siteData } = usePageData()
   const nav = siteData.themeConfig.nav || []
   return (
-    <header fixed="~" pos="t-0 l-0" w="full">
-      <div flex="~" items="center" justify="between" className="px-8 h-14 divider-bottom">
-        <div>
-          <a
-            href="/"
-            hover="opacity-60"
-            className="w-full h-full text-1rem font-semibold flex items-center"
-          >
-            Island.js
-          </a>
-        </div>
-        <ul flex="~">
-          <li flex="~">
-            {nav.map((item) => (
-              <MenuItem {...item} key={item.text} />
-            ))}
-          </li>
-          <li before="menu-item-before" flex="~">
-            <SwitchAppearance />
-          </li>
-          <li className={styles.socialLinkIcon} before="menu-item-before">
-            <a href="/">
-              <div className="i-carbon-logo-github w-5 h-5 fill-current"></div>
+    <div relative="~">
+      <header fixed="~" pos="t-0 l-0" w="full" bg="white">
+        <div flex="~" items="center" justify="between" className="px-8 h-14 divider-bottom">
+          <div>
+            <a
+              href="/"
+              hover="opacity-60"
+              className="w-full h-full text-1rem font-semibold flex items-center"
+            >
+              Island.js
             </a>
-          </li>
-        </ul>
-      </div>
-    </header>
+          </div>
+          <ul flex="~">
+            <li flex="~">
+              {nav.map((item) => (
+                <MenuItem {...item} key={item.text} />
+              ))}
+            </li>
+            <li before="menu-item-before" flex="~">
+              <SwitchAppearance />
+            </li>
+            <li className={styles.socialLinkIcon} before="menu-item-before">
+              <a href="/">
+                <div className="i-carbon-logo-github w-5 h-5 fill-current"></div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
   )
 }
