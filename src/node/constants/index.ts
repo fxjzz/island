e57b63dd6b7f362efb0119a292ca44ade1a73ed2
fxjzz <1042183935@qq.com>
2023-10-08 16:07:57 +0800

    babel编译时 island

--- src/node/constants/index.ts
+++ src/node/constants/index.ts
@@ -7,3 +7,5 @@ export const DEFAULT_TEMPLATE_PATH = join(PACKAGE_ROOT, 'template.html')
 export const CLIENT_ENTRY_PATH = join(PACKAGE_ROOT, 'src', 'runtime', 'client-entry.tsx')
 
 export const SSR_ENTRY_PATH = join(PACKAGE_ROOT, 'src', 'runtime', 'ssr-entry.tsx')
+
+export const MASK_SPLITTER = '!!ISLAND!!'
